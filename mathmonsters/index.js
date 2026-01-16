eaaff765e08719d227e2660ef7acc1e6c422ef46
joshleavitt1 <josh@joshleavitt.com>
2026-01-15 21:20:04 -0500

    Update index.js

--- mathmonsters/index.js
+++ mathmonsters/index.js
@@ -2223,7 +2223,7 @@ if (spinnerEl) spinnerEl.style.setProperty("--p", "0");
 
   // Builder bypass: add ?builder to return to the legacy landing screen.
   const isBuilderBypass = () =>
-    new URLSearchParams(window.location.search).has("dev");
+    new URLSearchParams(window.location.search).has("builder");
 
   async function initApp() {
     if (isBuilderBypass()) {
